c33c9a1b9c94990acc5a0a1f23ff19db58e901e5
Kevin Dixon <gaiwecoor@gmail.com>
2020-10-28 11:36:41 -0500

    Initial Twitch integration

--- utils/db.js
+++ utils/db.js
@@ -19,6 +19,7 @@ const users = {
     } else {
       return db.get("users").push({
         discordId: user,
+        channel: null,
         participant: null,
         team: null
       }).write();
@@ -27,6 +28,11 @@ const users = {
   getUser: (user) => {
     if (user.id) user = user.id;
     return db.get("users").find({discordId: user}).value();
+  },
+  setChannel: (user, channel) => {
+    if (user.id) user = user.id;
+    if (!db.get("users").find({discordId: user}).value()) users.addUser(user);
+    return db.get("users").find({discordId: user}).assign({channel}).write();
   },
   setParticipant: (user, id) => {
     if (user.id) user = user.id;
@@ -37,6 +43,14 @@ const users = {
     if (user.id) user = user.id;
     if (!db.get("users").find({discordId: user}).value()) users.addUser(user);
     return db.get("users").find({discordId: user}).assign({team: id}).write();
+  },
+  updateUser: (user, data) => {
+    if (user.id) user = user.id;
+    if (!db.get("users").find({discordId: user}).value()) users.addUser(user);
+    let update = {};
+    for (let prop of ["channel", "participant", "team"])
+      if (data[prop]) update[prop] = data[prop];
+    return db.get("users").find({discordId: user}).assign(update).write();
   }
 };
 
@@ -50,6 +64,9 @@ const servers = {
       return db.get("servers").push({
         id: guild,
         prefix: config.prefix,
+        channel: null,
+        notifications: null,
+        participant: null,
         team: null
       }).write();
     }
@@ -58,9 +75,28 @@ const servers = {
     if (guild.id) guild = guild.id;
     return db.get("servers").find({id: guild}).get("prefix").value();
   },
+  getServer: (guild) => {
+    if (guild.id) guild = guild.id;
+    return db.get("servers").find({id: guild}).value();
+  },
   getTeam: (guild) => {
     if (guild.id) guild = guild.id;
     return db.get("servers").find({id: guild}).get("team").value();
+  },
+  setChannel: (guild, channel) => {
+    if (guild.id) guild = guild.id;
+    if (!db.get("servers").find({id: guild}).value()) servers.addServer(guild);
+    return db.get("servers").find({id: guild}).assign({channel}).write();
+  },
+  setNotifications: (guild, notifications) => {
+    if (guild.id) guild = guild.id;
+    if (!db.get("servers").find({id: guild}).value()) servers.addServer(guild);
+    return db.get("servers").find({id: guild}).assign({notifications}).write();
+  },
+  setParticipant: (guild, participant) => {
+    if (guild.id) guild = guild.id;
+    if (!db.get("servers").find({id: guild}).value()) servers.addServer(guild);
+    return db.get("servers").find({id: guild}).assign({participant}).write();
   },
   setPrefix: (guild, prefix) => {
     if (guild.id) guild = guild.id;
@@ -71,6 +107,14 @@ const servers = {
     if (guild.id) guild = guild.id;
     if (!db.get("servers").find({id: guild}).value()) servers.addServer(guild);
     return db.get("servers").find({id: guild}).assign({team}).write();
+  },
+  updateServer: (guild, data) => {
+    if (guild.id) guild = guild.id;
+    if (!db.get("servers").find({id: guild}).value()) servers.addServer(guild);
+    let update = {};
+    for (let prop of ["prefix", "channel", "notifications", "participant", "team"])
+      if (data[prop]) update[prop] = data[prop];
+    return db.get("servers").find({id: guild}).assign(update).write();
   }
 };
 
